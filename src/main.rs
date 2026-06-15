7ad75529643f7e53ef2b3ce93ca76744e8bc3d96
0xMush <mushaibali7t1@gmail.com>
2026-06-15 19:13:41 -0400

    vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+
 // disables standard library
 #![no_std]
 // now we dont need crt0 to start runtime
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -12,19 +14,10 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // we are now overwriting the operating system entry point with our own _start function:
-static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    //Memory address 0xb8000 = screen memory
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
