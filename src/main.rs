27ec664da48de17e8bce42a1c0951fdbfd412b6f
0xMush <mushaibali7t1@gmail.com>
2026-06-14 22:07:44 -0400

    Printing Using VGA

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-
 // disables standard library
 #![no_std]
 // now we dont need crt0 to start runtime
@@ -14,7 +12,19 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // we are now overwriting the operating system entry point with our own _start function:
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    //Memory address 0xb8000 = screen memory
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
